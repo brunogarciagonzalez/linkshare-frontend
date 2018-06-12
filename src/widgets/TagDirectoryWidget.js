7028f01546cc5ab88f441cc32c7967559933c6c4
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-12 17:13:03 -0400

    advance HomePage

--- src/widgets/TagDirectoryWidget.js
+++ src/widgets/TagDirectoryWidget.js
@@ -8,7 +8,28 @@ class TagDirectoryWidget extends React.Component {
   }
 
   render() {
-    return <div />;
+    return (
+      <div className="ui grid">
+        <div className="three wide column" />
+
+        <div className="ten wide column">
+          <div className="ui segment">
+            <div className="ui large header centered">Explore Tags</div>
+            <div className="ui four column grid">
+              {this.props.tags.map(tag => {
+                return (
+                  <div className="column" key={tag.id}>
+                    {tag.title}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+
+        <div className="three wide column" />
+      </div>
+    );
   }
 }
 
