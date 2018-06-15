7b5b53decebcfd035cc7c37a2224a5bee688d137
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-15 12:28:42 -0400

    moved common divs into homepage

--- src/widgets/SearchWidget.js
+++ src/widgets/SearchWidget.js
@@ -63,36 +63,28 @@ class SearchWidget extends React.Component {
 
   render() {
     return (
-      <div className="ui grid">
-        <div className="three wide column" />
-
-        <div className="ten wide column">
-          <div className="ui segment">
-            <div className="ui large header centered">Search Tags</div>
-            <div className="ui search">
-              <div className="ui icon input fluid">
-                <input
-                  className="prompt"
-                  type="text"
-                  placeholder={this.state.randomTagName}
-                  value={this.state.currentSearchTerm}
-                  onChange={this.handleSearchChange}
-                  onKeyPress={this.handleSearchSubmit}
-                />
-                <i className="search icon" />
-              </div>
-              {this.state.searched ? (
-                <SearchResultsWidget
-                  loading={this.state.resultsLoading}
-                  searched={this.state.searchedTerm}
-                  results={this.state.results}
-                />
-              ) : null}
-            </div>
+      <div className="ui segment">
+        <div className="ui large header centered">Search Tags</div>
+        <div className="ui search">
+          <div className="ui icon input fluid">
+            <input
+              className="prompt"
+              type="text"
+              placeholder={this.state.randomTagName}
+              value={this.state.currentSearchTerm}
+              onChange={this.handleSearchChange}
+              onKeyPress={this.handleSearchSubmit}
+            />
+            <i className="search icon" />
           </div>
+          {this.state.searched ? (
+            <SearchResultsWidget
+              loading={this.state.resultsLoading}
+              searched={this.state.searchedTerm}
+              results={this.state.results}
+            />
+          ) : null}
         </div>
-
-        <div className="three wide column" />
       </div>
     );
   }
