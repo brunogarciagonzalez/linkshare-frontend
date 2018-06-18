cc7deec5d9a678468dab6209db54527c505e15f5
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-18 16:43:44 -0400

    completed about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,37 +9,49 @@ class AboutPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>About Page</h1>
-        <h1>App Vision: some new ideas</h1>
-        <p>Safe Browsing Check</p>
-        <ul>
-          <li>
-            <a href="https://developers.google.com/safe-browsing/v3/lookup-guide#ProtocolSpecification">
-              Google Safe Browsing Api
-            </a>
-          </li>
-          <li>
-            <a href="https://safeweb.norton.com/">Norton Safe Web</a>
-          </li>
-        </ul>
-        <p>Twitter API idea</p>
-        <ul>
-          <li>In order to bring users to the app / populate the database</li>
-          <li>
-            potential Twitter integration where people can send reviews via some
-            twitter format to the 'linkshare' twitter via DMs
-          </li>
-          <li>
-            the software could connect to the twitter api and log in as
-            'linkshare'
-          </li>
-          <li>
-            {" "}
-            and produce a user_share via some formatting / parsing of the DM
-            text
-          </li>
-        </ul>
+      <div className="ui grid">
+        <div className="three wide column" />
+
+        <div className="ten wide column">
+          <div className="ui segment">
+            <p>
+              This web application is powered by a Ruby on Rails (API), React,
+              and Semantic UI.
+            </p>
+            <p>The concept-board for this project includes three things:</p>
+            <ol>
+              <li>The now-gone Yahoo! Directory,</li>
+              <li>The frontpage of the internet: Reddit, and</li>
+              <li>Amazon product reviews</li>
+            </ol>
+            <p>
+              These three concepts are fused to produce a directory of the
+              internet, with admin-curated sub-directories (i.e. ‘tags’) that
+              are filled with links via user-generated content. These links are
+              each shared through the construction of reviews that have
+              qualitative and quantitative aspects (written content and
+              numerical rating, respectively).
+            </p>
+            <p>
+              In the end, it is an attempt at ‘community-based internet
+              exploration’, a concept I came up with to differentiate this idea
+              from crawler-based search engines as a means to navigate the
+              internet. The product is akin to having access to the locals and
+              their opinions when it comes to experiencing a new country, as
+              opposed to experiencing the country via conventional tours. At
+              some point, after goin through the tourist-experience many times
+              over, you may want to discover the hidden gems of the world wide
+              web.
+            </p>
+            <p>-- Bruno Garcia Gonzalez, June 2018</p>
+            <p>
+              <a href="https://www.linkedin.com/in/garcia-gonzalez">LinkedIn</a>
+              {" // "}
+              <a href="https://github.com/brunogarciagonzalez">GitHub</a>
+            </p>
+          </div>
+        </div>
+        <div className="three wide column" />
       </div>
     );
   }
